c7048aa0629300a54e0cd6cec3815fe1f0689dd7
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2020-05-08 12:54:17 +0300

    Added license header in source code files https://privatus.atlassian.net/browse/WC-924

--- common/IVPN Common/Models/PrivateEmail/PrivateEmailInfo.cs
+++ common/IVPN Common/Models/PrivateEmail/PrivateEmailInfo.cs
@@ -1,3 +1,25 @@
+//
+//  IVPN Client Desktop
+//  https://github.com/ivpn/desktop-app-ui
+//
+//  Created by Stelnykovych Alexandr.
+//  Copyright (c) 2020 Privatus Limited.
+//
+//  This file is part of the IVPN Client Desktop.
+//
+//  The IVPN Client Desktop is free software: you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//  The IVPN Client Desktop is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+//  details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the IVPN Client Desktop. If not, see <https://www.gnu.org/licenses/>.
+//
+
 ﻿namespace IVPN.Models.PrivateEmail
 {
     /// <summary>
