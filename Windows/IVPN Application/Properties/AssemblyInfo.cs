5206e0144d8a15068bf016b15189f05681720d10
Alexandr Stelnykovych <alexandr.stelnykovych@ivpn.net>
2020-09-02 10:00:37 +0300

    v2.12.6

--- Windows/IVPN Application/Properties/AssemblyInfo.cs
+++ Windows/IVPN Application/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.5.3")]
+[assembly: AssemblyVersion("2.12.6")]
 [assembly: InternalsVisibleTo("IVPN Application.Tests")]
